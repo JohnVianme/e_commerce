8adce88d356909acfd1b9d0afa7ec39ad0a73a26
Cesar Quihuis-Romero <cesar.d.quihuis.romero@gmail.com>
2025-11-25 15:21:43 -0700

    updated html and server(romero) to handle login and accout creation action events, todo: add more server side code to actually create accounts and update users file

--- server(romero).js
+++ server(romero).js
@@ -20,6 +20,31 @@ app.get('/login',(req,res)=>{
 	res.sendFile(path.join(dir,'login.html'))
 })
 
+app.post('/lgn_action', express.json(), (req,res)=>{
+	var query = req.body
+	var username = query.username
+	var password = query.password
+	var acctType = query.acct_type
+	console.log(query)
+	console.log(`Server says: username: ${username}, password: ${password}, acctType: ${acctType}`)
+})	
+
+app.post('/create_acct', express.json(), (req,res)=>{
+	var query = req.body
+	var username = query.username
+	var password = query.password
+	var acctType = query.acct_type
+	console.log(query)
+	console.log(`Server says: username: ${username}, password: ${password}, acctType: ${acctType}`)
+	res.send(`
+		<!DOCTYPE html>
+		<html>
+			<body>
+				A ${acctType} account created with username: ${username} 
+			</body>
+		</html>
+		`)
+})
 app.listen(port,()=>{
 	console.log('Server is running...')
 })
