1a8d101c455aaadd9269a9c2e4e870e9de505ae2
John Imanishimwe <148114742+JohnVianme@users.noreply.github.com>
2025-12-01 18:39:16 -0700

    added function to show collections

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const URL = "mongodb://localhost:27017";
 const client = new MongoClient(URL);
-
+let shopDB; // data base
 /*
 async promise connection
 */
@@ -28,21 +28,47 @@ async await connection
 async function MongoConnectAwait() {
   try {
     await client.connect();
+    shopDB = client.db("shopDB");
     console.log("Connected to Mangodb");
-    await client.close();
+    // await client.close();
   } catch (err) {
     console.log(err);
   }
 }
 
+async function showConsumers() {
+  try {
+    const producers = shopDB.collection("consumer");
+    const data = await producers.find({}).toArray();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function showProduct() {
+  try {
+    const producers = shopDB.collection("product");
+    const data = await producers.find({}).toArray();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/* 
+
+*/
 async function loadServer() {
   try {
-    // connect to the DB
-    MongoConnectAwait();
     // start the server
     app.listen(8080, () => {
       console.log(`Listening on port 8080...`);
     });
+
+    // connect to the DB
+    await MongoConnectAwait();
+    await showProduct();
   } catch (error) {
     // log issues with connecting
     console.log("Unable to Connect to Server!");
