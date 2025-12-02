61de5f46ffb5dd27c3149a20ffe31fa2a21b437e
John Imanishimwe <148114742+JohnVianme@users.noreply.github.com>
2025-12-01 17:27:48 -0700

    adding loadServer()

--- server.js
+++ server.js
@@ -2,14 +2,52 @@
 Test Server to Connect to MangoDB
 */
 const express = require("express");
-const crypto = require("crypto");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const URL = "mongodb://localhost:27017";
+const client = new MongoClient(URL);
 
-app.listen(8080, () => {
-  console.log(`Listening on port 8080`);
-});
+/*
+async promise connection
+*/
+// function MongoConnectPromise() {
+//   client
+//     .connect()
+//     .then(() => {
+//       console.log("Connected to MongoDB");
+//     })
+//     .catch((err) => console.log(err))
+//     .finally(() => {
+//       client.close();
+//     });
+// }
 
-// get home page
-app.get("/", (req, res) => {});
+/*
+async await connection
+*/
+async function MongoConnectAwait() {
+  try {
+    await client.connect();
+    console.log("Connected to Mangodb");
+    await client.close();
+  } catch (err) {
+    console.log(err);
+  }
+}
 
+async function loadServer() {
+  try {
+    // connect to the DB
+    MongoConnectAwait();
+    // start the server
+    app.listen(8080, () => {
+      console.log(`Listening on port 8080...`);
+    });
+  } catch (error) {
+    // log issues with connecting
+    console.log("Unable to Connect to Server!");
+    console.log(error);
+  }
+}
 
+loadServer();
